860c1e5675bfd943b18f551701051aaddb8e3c09
tw1ttt3r <tw1ttt3r@gmail.com>
2022-09-13 20:52:05 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,28 @@
+const elements = document.querySelectorAll(".img-container");
+
+console.log("elements", elements)
+
+// elements.map( (nodo) => {
+//  console.log("nodo", nodo)
+// })
+
+// const nuevoArreglo = Array.of(elements);
+
+// console.log("nuevoArreglo", nuevoArreglo)
+
+// nuevoArreglo.map( (nodo) => {
+//   console.log("nodo", nodo)
+//  })
+
+
+const button = document.querySelector("button");
+
+function aleatorio() {
+  return Math.random() * (9 - 1) + 1;
+}
+
+
+button.addEventListener("click", function() {
+  elements[0].style.gridArea = "elem1"
+})
+
